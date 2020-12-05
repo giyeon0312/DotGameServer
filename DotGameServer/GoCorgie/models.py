2acd621682a75aa8ec84d43c965fca6123fcdd60
giyeon0312 <giyeon0312@gmail.com>
2020-12-05 23:31:39 +0900

    Change to use USER model

--- DotGameServer/GoCorgie/models.py
+++ DotGameServer/GoCorgie/models.py
@@ -1,10 +1,47 @@
 from django.db import models
+from django.contrib import admin
+from django.contrib.auth.models import User
+from django.conf import settings
+import uuid
+import os
 
 # Create your models here.
-class Player(models.Model):
-    id=models.CharField(primary_key=True,max_length=20)
-    password=models.CharField(max_length=20)
-    score=models.FloatField(default=0)
-     #admin페이지에서 ID 뜨길 원하므로
-    def __str__(self):
-        return self.id
+
+class Score(models.Model):
+    owner=models.ForeignKey(User, on_delete=models.CASCADE)
+    score=models.IntegerField()
+    created=models.DateTimeField(auto_now_add=True)
+    updated=models.DateTimeField(auto_now=True)
+
+    def owner_name(self):
+        return self.owner.username
+
+    def __unicode__(self):
+        return '%s - %d' % (self.owner.username,self.score)
+
+#필요에 따라
+class Savegame(models.Model):
+    def update_filename(instance,filename):
+        #MEDIA_ROOT/savegames/ownername/uuid에 저장된다
+        path="savegames/"
+        format="%s%s" %(instance.owner.pk,str(uuid.uuid4))# 범용고유식별자
+        return os.path.join(path,format)
+
+    owner=models.ForeignKey(User, on_delete=models.CASCADE)
+    name=models.CharField(max_length=100)
+    file=models.FileField(upload_to=update_filename)
+    created=models.DateTimeField(auto_now_add=True)
+    updated=models.DateTimeField(auto_now=True)
+    type=models.CharField(max_length=100)
+
+    def __unicode__(self):
+        return '%s - %s' % (self.name, self.updated)
+
+class SavegameAdmin(admin.ModelAdmin):
+    fields = ('owner', 'name', 'file')
+    list_display = ['id', 'owner', 'name', 'created', 'updated']
+
+
+#admin에 등록
+admin.site.register(Score)
+admin.site.register(Savegame, SavegameAdmin)
--- DotGameServer/GoCorgie/serializers.py
+++ DotGameServer/GoCorgie/serializers.py
@@ -1,8 +1,33 @@
-from rest_framework import serializers
+from django.contrib.auth.models import User
+from rest_framework import serializers, filters
+from .models import Score,Savegame
 
-from .models import Player
 
-class PlayerSerializer(serializers.ModelSerializer):
+class ScoreSerializer(serializers.ModelSerializer):
     class Meta:
-        model=Player
-        fields = ('id','password','score')
+        model = Score
+        fields = ('id', 'score', 'owner_name', 'updated')
+
+
+class CreateUserSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField(required=True)
+
+    class Meta:
+        model = User
+        fields = ('id', 'email', 'username', 'password')
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = User(
+            email=validated_data['email'],
+            username=validated_data['username']
+        )
+        user.set_password(validated_data['password'])
+        user.save() 
+        return user
+
+
+class SavegameSerializer(serializers.ModelSerializer):
+    class Meta:
+        fields = ('id', 'name', 'updated', 'file', 'type')
+        model = Savegame
